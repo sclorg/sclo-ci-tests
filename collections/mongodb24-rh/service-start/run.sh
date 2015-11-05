393dcbe711d7a67535f709841e2f4f4934598cc7
Honza Horak <hhorak@redhat.com>
2015-11-05 21:00:39 +0100

    Use smallfiles for mongodb

--- collections/mongodb24-rh/service-start/run.sh
+++ collections/mongodb24-rh/service-start/run.sh
@@ -1,1 +1,9 @@
-../../common-scripts/service-start/run.sh
+h!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+echo "smallfiles = true" >>/opt/rh/rh-mongodb26/root/etc/mongodb.conf
+service "$SERVICE_NAME" start
+exit $?
--- collections/rh-mongodb26-rh/service-start/run.sh
+++ collections/rh-mongodb26-rh/service-start/run.sh
@@ -1,1 +1,9 @@
-../../common-scripts/service-start/run.sh
+h!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+echo "smallfiles = true" >>/etc/opt/rh/rh-mongodb26/mongod.conf
+service "$SERVICE_NAME" start
+exit $?
