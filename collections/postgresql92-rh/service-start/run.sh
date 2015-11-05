68eba11a0994b8ed9d759381be424d1d94b8af44
Honza Horak <hhorak@redhat.com>
2015-11-05 20:52:46 +0100

    Fix service start for postgresql 9.2

--- collections/postgresql92-rh/service-start/run.sh
+++ collections/postgresql92-rh/service-start/run.sh
@@ -4,6 +4,6 @@ THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../../common/functions.sh
 source ${THISDIR}/../include.sh
 
-scl enable $ENABLE_SCLS -- postgresql-setup --initdb
+scl enable $ENABLE_SCLS -- postgresql-setup initdb
 service "$SERVICE_NAME" start
 exit $?
