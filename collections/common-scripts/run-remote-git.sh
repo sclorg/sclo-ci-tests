dee103fa2d8c931bf92722ae5e6faced67c4ee34
Honza Horak <hhorak@redhat.com>
2015-11-05 17:58:55 +0100

    Guess IP from VM_NAME if given

--- collections/common-scripts/run-remote-git.sh
+++ collections/common-scripts/run-remote-git.sh
@@ -7,8 +7,39 @@
 THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../common/functions.sh
 
-if ! [ -v SSH_HOST ] ; then echo "Missing SSH_HOST variable, set it" ; exit 1 ; fi
-if ! [ -v SSH_PASS ] ; then echo "Missing SSH_PASS variable, set it" ; exit 1 ; fi
+# if SSH_HOST not set, get it from VM_NAME variable
+if ! [ -v SSH_HOST ] ; then
+  if ! [ -v VM_NAME ] ; then
+    echo "Missing SSH_HOST and VM_NAME variable, set at least one of them."
+    exit 1
+  fi
+
+  virsh list 2>/dev/null >/dev/null
+  if [ $? -eq 1 ] ; then
+    sudo='sudo '
+    echo "Connection to virtual machine requires root permissions:"
+  fi
+
+  if ! mac=$($sudo virsh --connect=qemu:///system  domiflist "$VM_NAME" 2>/dev/null \
+            |grep -o -E "([0-9a-f]{2}:){5}([0-9a-f]{2})") ; then
+    echo "$0: Failed to find virtual machine: $VM_NAME"
+    exit 3
+  fi
+
+  # Get IP
+  if ! SSH_HOST=$(arp -e |grep -e "$mac"  \
+            |grep -o -P "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}") ; then
+    echo "$0: Failed to get ip of the client in arp table"
+    exit 4
+  fi
+  echo Found $SSH_HOST
+fi
+
+if ! [ -v SSH_PASS ] ; then
+  echo "Missing SSH_PASS variable, set it"
+  exit 1
+fi
+
 SSH_USER=${SSH_USER-root}
 
 function sshwp
@@ -27,5 +58,4 @@ git clone $git_url \$repo/repo
 cd \$repo/repo/$rel_path
 ./run.sh
 "
-
 
