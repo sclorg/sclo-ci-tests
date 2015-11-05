188e620b612937d6726c64fcb207f89ecc815ccd
Honza Horak <hhorak@redhat.com>
2015-11-05 21:03:04 +0100

    Fix config name for mongodb24

--- collections/mongodb24-rh/service-start/run.sh
+++ collections/mongodb24-rh/service-start/run.sh
@@ -4,6 +4,6 @@ THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../../common/functions.sh
 source ${THISDIR}/../include.sh
 
-echo "smallfiles = true" >>/opt/rh/rh-mongodb26/root/etc/mongodb.conf
+echo "smallfiles = true" >>/opt/rh/mongodb24/root/etc/mongodb.conf
 service "$SERVICE_NAME" start
 exit $?
