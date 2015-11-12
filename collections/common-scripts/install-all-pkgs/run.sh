c8f49df20a3ac449e0e636850aa996f88d4108a2
Honza Horak <hhorak@redhat.com>
2015-11-12 22:20:07 +0100

    Add install all pkgs test

---
+++ collections/common-scripts/install-all-pkgs/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Caution: This is common script that is shared by more SCLS.
+# If you need to do changes related to this particular collection,
+# create a copy of this file instead of symlink.
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+generate_repo_file "$INSTALL_SCLS"
+
+set -e
+
+for SCL in $INSTALL_SCLS ; do
+  yum install -y ${SCL}\*
+done
