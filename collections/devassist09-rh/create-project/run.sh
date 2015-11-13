4db77fa43399deeedbe23ee09bf853a0a0533708
Honza Horak <hhorak@redhat.com>
2015-11-13 08:40:59 +0100

    Use variable to enable collections

--- collections/devassist09-rh/create-project/run.sh
+++ collections/devassist09-rh/create-project/run.sh
@@ -4,7 +4,7 @@ THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../../common/functions.sh
 source ${THISDIR}/../include.sh
 
-scl enable devassist09 - <<"EOF"
+scl enable $INSTALL_SCLS - <<"EOF"
   set -ex
   workkingdir=$(mktemp -d /tmp/da-prj-XXXXXX)
   pushd $workkingdir
