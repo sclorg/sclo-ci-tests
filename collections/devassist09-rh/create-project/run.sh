ce18a3e98f3f53eb24c2efa91d4153b2a545ab22
Honza Horak <hhorak@redhat.com>
2015-11-13 08:22:36 +0100

    Add create project test to devassist09

---
+++ collections/devassist09-rh/create-project/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+scl enable devassist09 - <<"EOF"
+  set -ex
+  workkingdir=$(mktemp -d /tmp/da-prj-XXXXXX)
+  pushd $workkingdir
+  da create python lib -n mypythonlib
+  ls ./mypythonlib/mypythonlib/__init__.py
+  popd
+EOF
+
