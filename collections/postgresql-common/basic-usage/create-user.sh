e8205a3d32238fa7531dbfa61405f91a680fc6c6
Honza Horak <hhorak@redhat.com>
2015-11-15 13:38:21 +0100

    Fix postgresql92 test

--- collections/postgresql-common/basic-usage/create-user.sh
+++ collections/postgresql-common/basic-usage/create-user.sh
@@ -7,17 +7,21 @@
 THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../../common/functions.sh
 
-set -xe
+set -x
 
 [ -z "${PGDATA}" ] && exit_fail "Environment variable PGDATA must be set"
 
 service $SERVICE_NAME restart
 
-su - postgres <<'EOF'
-source scl_source enable $ENABLED_SCLS
-psql -c "CREATE ROLE testuser ENCRYPTED PASSWORD 'secretpass' LOGIN;"
-createdb testdb --owner testuser;
+su - postgres <<EOF
+  set -x
+  source scl_source enable ${ENABLED_SCLS}
+  psql -c "CREATE ROLE testuser ENCRYPTED PASSWORD 'secretpass' LOGIN;"
+  createdb testdb --owner testuser;
 EOF
+
+# we can ignore errors until now
+set -e
 
 echo "local all testuser md5" > ${PGDATA}/data/pg_hba.conf
 service $SERVICE_NAME restart
--- collections/postgresql92-rh/include.sh
+++ collections/postgresql92-rh/include.sh
@@ -1,4 +1,4 @@
 export INSTALL_SCLS=postgresql92
 export ENABLE_SCLS=postgresql92
 export SERVICE_NAME=postgresql92-postgresql
-export PGDATA=/opt/rh/rh-postgresql94/root/var/lib/pgsql
+export PGDATA=/opt/rh/postgresql92/root/var/lib/pgsql
