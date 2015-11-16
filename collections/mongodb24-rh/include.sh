b7a280c892465001b849dedf37d49c49abce4681
Honza Horak <hhorak@redhat.com>
2015-11-16 23:21:34 +0100

    Fix exclude def in mongodb24

--- collections/mongodb24-rh/include.sh
+++ collections/mongodb24-rh/include.sh
@@ -1,5 +1,5 @@
 export INSTALL_SCLS=mongodb24
 export INSTALL_PKGS="mongodb24 mongodb24-mongodb"
-export EXCLUDE_PKGS="--exclude mongodb24-build --exclude mongodb24-scldevel mongodb24-gperftools-pprof"
+export EXCLUDE_PKGS="--exclude mongodb24-build --exclude mongodb24-scldevel --exclude mongodb24-gperftools-pprof"
 export ENABLE_SCLS=mongodb24
 export SERVICE_NAME=mongodb24-mongodb
