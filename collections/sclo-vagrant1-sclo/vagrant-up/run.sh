fe0746b81407f33df79bf2cead7fcec73a25317b
Dominic Cleal <dcleal@redhat.com>
2016-01-27 08:36:00 +0000

    Fix assumption about PWD and test directory
    Fixes #11

--- collections/sclo-vagrant1-sclo/vagrant-up/run.sh
+++ collections/sclo-vagrant1-sclo/vagrant-up/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+
 # we need to run this as root
 
 service libvirtd start
 syncdir=$(mktemp -d /tmp/sync-XXXXXX)
 yum -y install git
 git clone https://github.com/CentOS/sig-core-t_functional $syncdir
-cp Vagrantfile $syncdir
-chmod u+x ./vagrant_test.sh
-scl enable sclo-vagrant1 ./vagrant_test.sh $syncdir
+cp $THISDIR/Vagrantfile $syncdir
+chmod u+x $THISDIR/vagrant_test.sh
+scl enable sclo-vagrant1 $THISDIR/vagrant_test.sh $syncdir
 exit $?
