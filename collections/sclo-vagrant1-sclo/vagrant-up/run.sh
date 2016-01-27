e8d41f04c318eb0d7c5588215139ba4ef1f032d9
Dominic Cleal <dcleal@redhat.com>
2016-01-27 08:40:40 +0000

    Fix bad quoting to scl enable

--- collections/sclo-vagrant1-sclo/vagrant-up/run.sh
+++ collections/sclo-vagrant1-sclo/vagrant-up/run.sh
@@ -10,5 +10,5 @@ yum -y install git
 git clone https://github.com/CentOS/sig-core-t_functional $syncdir
 cp $THISDIR/Vagrantfile $syncdir
 chmod u+x $THISDIR/vagrant_test.sh
-scl enable sclo-vagrant1 $THISDIR/vagrant_test.sh $syncdir
+scl enable sclo-vagrant1 "$THISDIR/vagrant_test.sh $syncdir"
 exit $?
