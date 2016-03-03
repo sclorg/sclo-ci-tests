d11098633da0f4860028ff5ee13d532284ad2d04
Honza Horak <hhorak@redhat.com>
2016-03-03 15:47:57 +0100

    Add tests for rh-muby23 and rh-ror42

---
+++ collections/rh-ror42-rh/include.sh
@@ -0,0 +1,2 @@
+export INSTALL_SCLS=rh-ror42
+export ENABLE_SCLS=rh-ror42
---
+++ collections/rh-ruby23-rh/include.sh
@@ -0,0 +1,3 @@
+export INSTALL_SCLS="rh-ruby23"
+export INSTALL_PKGS="rh-ruby23 rh-ruby23-ruby-devel"
+export ENABLE_SCLS=rh-ruby23
