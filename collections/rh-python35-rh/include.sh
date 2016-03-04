9bed92684e01320813ff8a81f49f3883dfc399e4
Honza Horak <hhorak@redhat.com>
2016-03-04 11:14:20 +0100

    Install rh-python35-python-jinja2 since we need it for Flask test

--- collections/rh-python35-rh/include.sh
+++ collections/rh-python35-rh/include.sh
@@ -1,3 +1,4 @@
-export INSTALL_SCLS=rh-python35
+export INSTALL_SCLS="rh-python35 httpd24"
+export INSTALL_PKGS="rh-python35 rh-python35-python-jinja2"
 export ENABLE_SCLS=rh-python35
 export EXCLUDE_PKGS="--exclude rh-python35-python-test --exclude rh-python35-python-debug"
