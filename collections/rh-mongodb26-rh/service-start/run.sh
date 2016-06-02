b439833ddae5df25ed6b98a59436b31b5fa3fd3b
Honza Horak <hhorak@redhat.com>
2016-06-02 16:33:47 +0200

    Do not add duplicit smallfiles option in mongodb

--- collections/rh-mongodb26-rh/service-start/run.sh
+++ collections/rh-mongodb26-rh/service-start/run.sh
@@ -4,6 +4,6 @@ THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../../common/functions.sh
 source ${THISDIR}/../include.sh
 
-echo "smallfiles = true" >>/etc/opt/rh/rh-mongodb26/mongod.conf
+grep -e '^[[:space:]]*smallfiles' /etc/opt/rh/rh-mongodb26/mongod.conf &>/dev/null || echo "smallfiles = true" >>/etc/opt/rh/rh-mongodb26/mongod.conf
 service "$SERVICE_NAME" start
 exit $?
--- collections/rh-mongodb30upg-rh/service-start/run.sh
+++ collections/rh-mongodb30upg-rh/service-start/run.sh
@@ -4,6 +4,6 @@ THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../../common/functions.sh
 source ${THISDIR}/../include.sh
 
-echo "smallfiles = true" >>/etc/opt/rh/rh-mongodb30upg/mongod.conf
+grep -e '^[[:space:]]*smallfiles' /etc/opt/rh/rh-mongodb30upg/mongod.conf &>/dev/null || echo "smallfiles = true" >>/etc/opt/rh/rh-mongodb30upg/mongod.conf
 service "$SERVICE_NAME" start
 exit $?
