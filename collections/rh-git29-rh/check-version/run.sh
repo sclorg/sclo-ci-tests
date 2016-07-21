63c8c3897e00c8114587f2da70fe4bd9f00236fc
Honza Horak <hhorak@redhat.com>
2016-07-21 17:19:38 +0200

    Add missing includes into check-version test of git29 scl

--- collections/rh-git29-rh/check-version/run.sh
+++ collections/rh-git29-rh/check-version/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
 out=$(scl enable $INSTALL_SCLS 'git --version' )
 ret=$?
 
