3fa2e57d5a1270423ee799efb102b2496839d598
Honza Horak <hhorak@redhat.com>
2016-10-24 23:05:30 +0200

    Add tests for rh-php70

---
+++ collections/rh-php70-rh/dynamic-content/run.sh
@@ -0,0 +1,1 @@
+../../php-httpd-common/dynamic-content/run.sh
---
+++ collections/rh-php70-rh/include.sh
@@ -0,0 +1,5 @@
+export INSTALL_SCLS=rh-php70
+export ENABLE_SCLS=rh-php70
+export PHP_HTTPD_PKGS="httpd24 rh-php70-php-fpm"
+export HTTPD_SERVICE_NAME="httpd24-httpd rh-php70-php-fpm"
+export STATIC_DATA_DIR=/opt/rh/httpd24/root/var/www/html
