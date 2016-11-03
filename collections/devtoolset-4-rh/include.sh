3cb010b414aa663bbe6c4162c88fc959622fa123
Honza Horak <hhorak@redhat.com>
2016-11-03 23:00:58 +0200

    Use INSTALL_PKGS for picking what should be installed

--- collections/devtoolset-4-rh/include.sh
+++ collections/devtoolset-4-rh/include.sh
@@ -1,2 +1,3 @@
-export INSTALL_SCLS="devtoolset-4-perftools devtoolset-4-toolchain"
+export INSTALL_SCLS=devtoolset-4
+export INSTALL_PKGS="devtoolset-4-perftools devtoolset-4-toolchain"
 export ENABLE_SCLS=devtoolset-4
