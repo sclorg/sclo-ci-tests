26d22f302d849c8366526f162c1b2d3756dce144
Honza Horak <hhorak@redhat.com>
2016-11-11 17:00:20 +0100

    Add tests for devtoolset-6 SCL

---
+++ collections/devtoolset-6-rh/compile-postgresql/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Caution: This is common script that is shared by more SCLS.
+# If you need to do changes related to this particular collection,
+# create a copy of this file instead of symlink.
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+# make sure we don't use system gcc
+#yum -y remove gcc || :
+# install dependencies for compilation process
+yum -y install wget bzip2-devel bzip2 coreutils cpio diffutils findutils gawk gcc gcc-c++ grep gzip info make patch redhat-release-server redhat-rpm-config rpm-build sed shadow-utils tar unzip util-linux-ng which iso-codes 'perl(ExtUtils::MakeMaker)' glibc-devel bison flex gawk 'perl(ExtUtils::Embed)' readline-devel zlib-devel
+
+scl enable $INSTALL_SCLS - <<"EOF"
+  set -ex
+  workkingdir=$(mktemp -d /tmp/da-prj-XXXXXX)
+  pushd $workkingdir
+  wget https://ftp.postgresql.org/pub/source/v9.4.4/postgresql-9.4.4.tar.bz2
+  tar -xf postgresql-9.4.4.tar.bz2
+  cd postgresql-9.4.4
+  ./configure --without-readline --without-zlib
+  make
+  gccver=$(strings -a ./src/backend/postgres | grep -oe "compiled by.*GCC) \([0-9]*\.[0-9]*\.\)" | sed -e 's/.*[[:space:]]//')
+  [ "${gccver}" != "5.2." ] && [ "${gccver}" != "5.3." ] && echo "Wrong GCC version found in postgres binary: ${gccver}" && exit 1
+  popd
+EOF
+
---
+++ collections/devtoolset-6-rh/include.sh
@@ -0,0 +1,3 @@
+export INSTALL_SCLS=devtoolset-6
+export INSTALL_PKGS="devtoolset-6-perftools devtoolset-6-toolchain"
+export ENABLE_SCLS=devtoolset-6
