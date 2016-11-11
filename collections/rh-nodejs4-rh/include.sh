7c5e0c9cf2228e9d35b78d137963183407f95c2b
Honza Horak <hhorak@redhat.com>
2016-11-11 15:43:10 +0100

    Exclude rh-nodejs4-libuv-static

--- collections/rh-nodejs4-rh/include.sh
+++ collections/rh-nodejs4-rh/include.sh
@@ -1,3 +1,4 @@
 export INSTALL_SCLS=rh-nodejs4
 export INSTALL_PKGS="rh-nodejs4 rh-nodejs4-npm rh-nodejs4-nodejs"
 export ENABLE_SCLS=rh-nodejs4
+export EXCLUDE_PKGS="--exclude=rh-nodejs4-libuv-static"
