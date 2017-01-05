8b31cfa36c06824d892a18b6a974c3d81b087b92
Honza Horak <hhorak@redhat.com>
2017-01-05 20:58:39 +0100

    Install also devtoolset-4-eclipse when testing devtoolset-4

--- collections/devtoolset-4-rh/include.sh
+++ collections/devtoolset-4-rh/include.sh
@@ -1,3 +1,3 @@
 export INSTALL_SCLS=devtoolset-4
-export INSTALL_PKGS="devtoolset-4-perftools devtoolset-4-toolchain devtoolset-4-ide"
+export INSTALL_PKGS="devtoolset-4-perftools devtoolset-4-toolchain devtoolset-4-ide devtoolset-4-eclipse"
 export ENABLE_SCLS=devtoolset-4
