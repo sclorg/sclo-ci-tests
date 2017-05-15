29555dd135ac7a9239b403bfaa1a83960e8f226e
Honza Horak <hhorak@redhat.com>
2017-05-15 21:39:45 +0200

    Add NodeJS 6 tests

---
+++ collections/rh-nodejs6-rh/include.sh
@@ -0,0 +1,4 @@
+export INSTALL_SCLS=rh-nodejs6
+export INSTALL_PKGS="rh-nodejs6 rh-nodejs6-npm rh-nodejs6-nodejs"
+export ENABLE_SCLS=rh-nodejs6
+#export EXCLUDE_PKGS="--exclude=rh-nodejs6-libuv-static"
