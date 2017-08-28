8c9d45e4ff02ea2e9f63ed915471c5fa5f61aa91
Honza Horak <hhorak@redhat.com>
2017-08-28 16:11:42 -0400

    Add variables into rh-mysql57-rh/include.sh

--- collections/rh-mysql57-rh/include.sh
+++ collections/rh-mysql57-rh/include.sh
@@ -1,3 +1,6 @@
 export INSTALL_SCLS=rh-mysql57
 export ENABLE_SCLS=rh-mysql57
 export SERVICE_NAME=rh-mysql57-mysqld
+export DATA_DIR=/var/opt/rh/rh-mysql57/lib/mysql
+export BASE_DIR=/opt/rh/rh-mysql57/root/usr
+export SYSCONF_DIR=/etc/opt/rh/rh-mysql57
