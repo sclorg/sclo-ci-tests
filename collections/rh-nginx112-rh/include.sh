49983d4de48837b6f7a8ccc2509d8adaa73d57d5
Jan Staněk <jstanek@redhat.com>
2017-09-01 13:15:06 +0200

    Add tests for rh-nginx112 [sclorg#25]

---
+++ collections/rh-nginx112-rh/include.sh
@@ -0,0 +1,5 @@
+export INSTALL_SCLS=rh-nginx112
+export ENABLE_SCLS=rh-nginx112
+export SERVICE_NAME=rh-nginx112-nginx
+export STATIC_DATA_DIR=/var/opt/rh/rh-nginx112/lib/nginx/public_html
+export CONFIG_D_DIR=/etc/opt/rh/rh-nginx112/nginx/conf.d
