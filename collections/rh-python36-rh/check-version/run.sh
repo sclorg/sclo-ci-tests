709aeda56df411b005e682c4014a47e4e281ae27
Jan Staněk <jstanek@redhat.com>
2017-09-01 13:09:31 +0200

    Add tests for rh-python36 [sclorg#25]

---
+++ collections/rh-python36-rh/check-version/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../include.sh
+
+out=$(scl enable $ENABLE_SCLS -- python -V 2>&1 )
+ret=$?
+
+echo "$out"|grep -o 'Python 3\.6\.'
+exit $retcode
+
---
+++ collections/rh-python36-rh/include.sh
@@ -0,0 +1,4 @@
+export INSTALL_SCLS="rh-python36 httpd24"
+export INSTALL_PKGS="rh-python36 rh-python36-python-jinja2"
+export ENABLE_SCLS=rh-python36
+export EXCLUDE_PKGS="--exclude rh-python36-python-test --exclude rh-python36-python-debug"
