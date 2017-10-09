ccd0c21f990ba06f15e4f8b1fa44ac7699f3b7db
Augusto Caringi <acaringi@redhat.com>
2017-10-09 14:21:27 +0200

    sclo-cassandra3: add basic-usage test

---
+++ collections/sclo-cassandra3-sclo/basic-usage/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+
+source scl_source enable ${ENABLE_SCLS}
+
+set -xe
+
+cqlsh -e "CREATE KEYSPACE IF NOT EXISTS k1 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}  AND durable_writes = true; USE K1; CREATE TABLE IF NOT EXISTS person (id text, name text, surname text, email text, PRIMARY KEY (id)); INSERT INTO person (id, name, surname, email) VALUES ('003', 'Harry', 'Potter', 'harry@example.com');"
+
+out=$(
+cqlsh -e "USE k1; SELECT count(*) FROM person WHERE id = '003'";
+)
+out=$(echo "$out"|head -4|tail -1|awk '{$1=$1};1')
+
+echo $out
+
+[ "$out" == "1" ]
+
--- collections/sclo-cassandra3-sclo/include.sh
+++ collections/sclo-cassandra3-sclo/include.sh
@@ -1,3 +1,4 @@
 export INSTALL_SCLS=sclo-cassandra3
+export INSTALL_PKGS='sclo-cassandra3-cassandra sclo-cassandra3'
 export ENABLE_SCLS=sclo-cassandra3
 export SERVICE_NAME=sclo-cassandra3-cassandra
