e3a511a1da4adedc18795f983d613f6492188d9d
Honza Horak <hhorak@redhat.com>
2017-10-25 08:27:30 +0200

    Do not install rh-mongodb34-netty with broken deps

--- collections/rh-mongodb34-rh/include.sh
+++ collections/rh-mongodb34-rh/include.sh
@@ -2,4 +2,4 @@ export INSTALL_SCLS=rh-mongodb34
 export INSTALL_PKGS="rh-mongodb34 rh-mongodb34-mongodb"
 export ENABLE_SCLS=rh-mongodb34
 export SERVICE_NAME=rh-mongodb34-mongod
-export EXCLUDE_PKGS="--exclude=rh-mongodb34-golang-github*devel --exclude=rh-mongodb34-mongo-tools-devel --exclude=rh-mongodb34-golang-github*unit-test --exclude=rh-mongodb34-mongo-tools-unit-test"
+export EXCLUDE_PKGS="--exclude=rh-mongodb34-golang-github*devel --exclude=rh-mongodb34-mongo-tools-devel --exclude=rh-mongodb34-golang-github*unit-test --exclude=rh-mongodb34-mongo-tools-unit-test --exclude=rh-mongodb34-netty"
