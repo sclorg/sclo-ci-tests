bd705be76d414298f97a988099e7aab43eb4c57f
root <root@qeos-45.lab.eng.rdu2.redhat.com>
2018-02-05 05:38:12 -0500

    Fix selinux test for haproxy

--- collections/rh-haproxy18-rh/selinux/run.sh
+++ collections/rh-haproxy18-rh/selinux/run.sh
@@ -2,20 +2,21 @@
 
 THISDIR=$(dirname ${BASH_SOURCE[0]})
 source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
 
 HAPLIBPATH="${LOCALSTATE_DIR}/lib/haproxy"
 
 retval=0
 
 while read path se_context ; do
-  if ! ls -lz "${path}" | grep "${se_context}" ; then
-    echo "File ${path} has wrong context, ${se_context} expected."
-    ls -lz "${path}"
+  if ! ls -ldZ "${path}" | grep -q "${se_context}" ; then
+    echo "File ${path} has wrong context, ${se_context} expected." >&2
+    ls -ldZ "${path}"
     retval=1
   fi
 done <<< "${HAPLIBPATH} haproxy_var_lib_t
 ${PREFIX}/sbin/haproxy haproxy_exec_t
-${SYSTEMD_UNIT} haproxy_unit_file_t"
+/usr/lib/systemd/system/${SYSTEMD_UNIT} haproxy_unit_file_t"
 
 exit $retval
 
