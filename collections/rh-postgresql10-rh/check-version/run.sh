053494930405d63503cd75de31e0a8f7390dfb50
Jan Staněk <jstanek@redhat.com>
2018-05-16 11:04:10 +0200

    Create tests for rh-postgresql10 collection

---
+++ collections/rh-postgresql10-rh/check-version/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+out=$(scl enable $ENABLE_SCLS 'postgres -V')
+retcode=$?
+
+echo "$out"|grep -o '^postgres (PostgreSQL) 10\.1'
+
+exit $retcode
---
+++ collections/rh-postgresql10-rh/include.sh
@@ -0,0 +1,4 @@
+export INSTALL_SCLS=rh-postgresql10
+export ENABLE_SCLS=rh-postgresql10
+export SERVICE_NAME=rh-postgresql10-postgresql
+export PGDATA=/var/opt/rh/rh-postgresql10/lib/pgsql
