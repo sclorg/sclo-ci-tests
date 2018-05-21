7db5ae03d5f3cf219bfc75bfb7eb69196da65dd2
Jan Staněk <jstanek@redhat.com>
2018-05-21 16:07:54 +0200

    Explicitly install main varnish package

--- collections/rh-varnish5-rh/include.sh
+++ collections/rh-varnish5-rh/include.sh
@@ -1,4 +1,5 @@
 export INSTALL_SCLS='rh-varnish5 httpd24'
+export INSTALL_PKGS='rh-varnish5-varnish httpd24-httpd'
 export ENABLE_SCLS=rh-varnish5
 export SERVICE_NAME='rh-varnish5-varnish httpd24-httpd'
 export STATIC_DATA_DIR=/opt/rh/httpd24/root/var/www/html/
