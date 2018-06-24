e519ee740c81f9277677e20ad8273c9ae7e7e2fd
Honza Horak <hhorak@redhat.com>
2018-06-24 15:21:29 -0400

    Install rh-mysql57-mysql so we really use the client from this SCL

--- collections/rh-mysql57-rh/include.sh
+++ collections/rh-mysql57-rh/include.sh
@@ -1,4 +1,5 @@
 export INSTALL_SCLS=rh-mysql57
+export INSTALL_PKGS="rh-mysql57 rh-mysql57-mysql"
 export ENABLE_SCLS=rh-mysql57
 export SERVICE_NAME=rh-mysql57-mysqld
 export DATA_DIR=/var/opt/rh/rh-mysql57/lib/mysql
