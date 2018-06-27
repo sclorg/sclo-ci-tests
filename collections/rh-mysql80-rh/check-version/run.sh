ccddbbeb5419e75c03b18077be09ee8175cb90bc
Honza Horak <hhorak@redhat.com>
2018-06-27 09:13:31 +0200

    Add rh-mysql80 SCL

---
+++ collections/rh-mysql80-rh/check-version/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+out=$(scl enable $ENABLE_SCLS 'mysql -V')
+retcode=$?
+
+echo "$out"|grep -o 'Distrib 8\.0\.'
+
+exit $retcode
---
+++ collections/rh-mysql80-rh/include.sh
@@ -0,0 +1,7 @@
+export INSTALL_SCLS=rh-mysql80
+export INSTALL_PKGS="rh-mysql80 rh-mysql80-mysql"
+export ENABLE_SCLS=rh-mysql80
+export SERVICE_NAME=rh-mysql80-mysqld
+export DATA_DIR=/var/opt/rh/rh-mysql80/lib/mysql
+export BASE_DIR=/opt/rh/rh-mysql80/root/usr
+export SYSCONF_DIR=/etc/opt/rh/rh-mysql80
