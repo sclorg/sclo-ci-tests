5112d3d2553fa72461acdbfb2cd60ee17f12f716
Remi Collet <remi@remirepo.net>
2018-10-30 16:16:40 +0100

    add test for rh-php72

---
+++ collections/rh-php72-rh/check-version/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+out=$(scl enable $ENABLE_SCLS "php -v")
+ret=$?
+
+echo "$out"|grep -o 'PHP 7\.1\.'
+exit $retcode
+
---
+++ collections/rh-php72-rh/include.sh
@@ -0,0 +1,5 @@
+export INSTALL_SCLS=rh-php71
+export ENABLE_SCLS=rh-php71
+export PHP_HTTPD_PKGS="httpd24 rh-php71-php-fpm"
+export HTTPD_SERVICE_NAME="httpd24-httpd rh-php71-php-fpm"
+export STATIC_DATA_DIR=/opt/rh/httpd24/root/var/www/html
