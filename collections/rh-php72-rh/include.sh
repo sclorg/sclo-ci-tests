3a4e13383848397d2af577be1df5dd0a58d51594
Remi Collet <remi@famillecollet.com>
2018-10-30 16:41:27 +0100

    add test for rh-php72 (#31)

---
+++ collections/rh-php72-rh/include.sh
@@ -0,0 +1,5 @@
+export INSTALL_SCLS=rh-php72
+export ENABLE_SCLS=rh-php72
+export PHP_HTTPD_PKGS="httpd24 rh-php72-php-fpm"
+export HTTPD_SERVICE_NAME="httpd24-httpd rh-php72-php-fpm"
+export STATIC_DATA_DIR=/opt/rh/httpd24/root/var/www/html
