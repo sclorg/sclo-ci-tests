4cc56e7c9b14186f809641b17f333b70dd67c460
Jan Staněk <jstanek@redhat.com>
2018-11-01 15:55:03 +0100

    Add tests for rh-nginx114

---
+++ collections/rh-nginx114-rh/include.sh
@@ -0,0 +1,5 @@
+export INSTALL_SCLS=rh-nginx114
+export ENABLE_SCLS=rh-nginx114
+export SERVICE_NAME=rh-nginx114-nginx
+export STATIC_DATA_DIR=/var/opt/rh/rh-nginx114/lib/nginx/public_html
+export CONFIG_D_DIR=/etc/opt/rh/rh-nginx114/nginx/conf.d
