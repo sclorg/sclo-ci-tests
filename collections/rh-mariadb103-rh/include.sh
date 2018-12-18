e12fa5196251b510ce252a5d6d4a714d7168c5b9
Honza Horak <hhorak@redhat.com>
2018-12-18 08:23:24 -0500

    First iteration of mariadb 10.3 tests

---
+++ collections/rh-mariadb103-rh/include.sh
@@ -0,0 +1,8 @@
+export INSTALL_SCLS=rh-mariadb103
+export ENABLE_SCLS=rh-mariadb103
+export SERVICE_NAME=rh-mariadb103-mariadb
+export SCL_GALERA_PKGS="rh-mariadb103-mariadb-server-galera rh-mariadb103-galera"
+export DATA_DIR=/var/opt/rh/rh-mariadb103/lib/mysql
+export BASE_DIR=/opt/rh/rh-mariadb103/root/usr
+export SYSCONF_DIR=/etc/opt/rh/rh-mariadb103
+export GARBD_SERVICE_NAME=rh-mariadb103-garbd
---
+++ collections/rh-mariadb103-rh/syspaths/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Caution: This is common script that is shared by more SCLS.
+# If you need to do changes related to this particular collection,
+# create a copy of this file instead of symlink.
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+source ${THISDIR}/../include.sh
+
+yum install -y ${YUM_OPTS:-} ${INSTALL_SCLS}*-syspaths
+
+service mariadb start
+
+echo 'select 1;' | mysql || exit $?
+man mysql >/dev/null || exit $?
+
+service mariadb stop
