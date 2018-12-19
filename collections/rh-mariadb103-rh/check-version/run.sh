5650ee490ef6d751ee58a4187f73f8fa520f6f15
Honza Horak <hhorak@redhat.com>
2018-12-19 03:24:55 -0500

    Fix version for mariadb 10.3

--- collections/rh-mariadb103-rh/check-version/run.sh
+++ collections/rh-mariadb103-rh/check-version/run.sh
@@ -8,6 +8,6 @@ source ${THISDIR}/../include.sh
 out=$(scl enable $ENABLE_SCLS 'mysql -V')
 retcode=$?
 
-echo "$out"|grep -o 'Distrib 10\.2\.'
+echo "$out"|grep -o 'Distrib 10\.3\.'
 
 exit $retcode
