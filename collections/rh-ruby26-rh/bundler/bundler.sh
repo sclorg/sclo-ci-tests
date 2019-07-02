f916605578b9eafba2e6b8008b1c3cf582d8b2fe
Jan Staněk <jstanek@redhat.com>
2019-07-02 14:43:13 +0200

    Add tests for rh-ruby26

---
+++ collections/rh-ruby26-rh/bundler/bundler.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# rh-ruby26 contains a bug in Bundler broke the common test case
+# Below is rh-ruby26-specific workaround
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+
+set -xe
+
+yum install -y ${INSTALL_SCLS}-rubygem-bundler
+
+username=testuser$$
+homedir=/var/lib/test-home$$
+mkdir -p $homedir
+useradd -d $homedir $username
+cp ${THISDIR}/Gemfile $homedir
+chown -R $username:$username $homedir
+su - $username -c "cd \$HOME ; scl enable $ENABLE_SCLS --  bundle --path \$(ruby -e 'Gem.user_dir')" || exit_fail "bundler failed"
+userdel -r $username
+
+exit $?
---
+++ collections/rh-ruby26-rh/include.sh
@@ -0,0 +1,4 @@
+export INSTALL_SCLS="rh-ruby26"
+export INSTALL_PKGS="rh-ruby26 rh-ruby26-ruby-devel"
+export ENABLE_SCLS=rh-ruby26
+export EXCLUDE_PKGS="--exclude=rh-ruby26-rubygem-bundler-doc"
