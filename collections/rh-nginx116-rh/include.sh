13d1edab930463becfa5496a2b8413638c76b17c
Honza Horak <hhorak@redhat.com>
2019-10-05 23:01:33 +0200

    Add rh-nginx116

---
+++ collections/rh-nginx116-rh/include.sh
@@ -0,0 +1,5 @@
+export INSTALL_SCLS=rh-nginx116
+export ENABLE_SCLS=rh-nginx116
+export SERVICE_NAME=rh-nginx116-nginx
+export STATIC_DATA_DIR=/var/opt/rh/rh-nginx116/lib/nginx/public_html
+export CONFIG_D_DIR=/etc/opt/rh/rh-nginx116/nginx/conf.d
