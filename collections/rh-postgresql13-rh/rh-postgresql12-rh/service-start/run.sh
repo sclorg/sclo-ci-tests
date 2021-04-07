aa26842f707e9ffc97dc45fb39bdb62d8367c8c0
Jan Staněk <jstanek@redhat.com>
2021-04-07 12:53:59 +0200

    remove rh-postgresql13-rh/rh-postgresql12-rh directory
    These two files seem to be included by accident;
    the broken symlink causes the CI to fail

--- collections/rh-postgresql13-rh/rh-postgresql12-rh/service-start/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-THISDIR=$(dirname ${BASH_SOURCE[0]})
-source ${THISDIR}/../../../common/functions.sh
-source ${THISDIR}/../include.sh
-
-scl enable $ENABLE_SCLS -- postgresql-setup --initdb
-service "$SERVICE_NAME" start
-exit $?
